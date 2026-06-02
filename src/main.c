746748369c46285b1894ded19409e8bfeb22bc55
agustin-197 <agustin041097@gmail.com>
2026-06-01 21:23:25 -0300

    Añadido de comentarios 2

--- src/main.c
+++ src/main.c
@@ -1,9 +1,51 @@
-/** * @file main.c
- * @brief Programa principal para probar la calculadora abstracta.
+/*********************************************************************************************************************
+Copyright 2016-2025, Laboratorio de Microprocesadores
+Facultad de Ciencias Exactas y Tecnología
+Universidad Nacional de Tucuman
+http://www.microprocesadores.unt.edu.ar/
+
+Copyright 2016-2025, Esteban Volentini <evolentini@herrera.unt.edu.ar>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+SPDX-License-Identifier: MIT
+*************************************************************************************************/
+
+/** @file main.c
+ ** @brief Programa principal para probar la calculadora abstracta.
  **/
+
+/* === Headers files inclusions ================================================================ */
 
 #include <stdio.h>
 #include "calculadora.h"
+
+/* === Macros definitions ====================================================================== */
+
+/* === Private data type declarations ========================================================== */
+
+/* === Private function declarations =========================================================== */
+
+/* === Private variable definitions ============================================================ */
+
+/* === Public variable definition  ============================================================= */
+
+/* === Private function definitions ============================================================ */
 
 /**
  * @brief Implementación de la operación suma.
@@ -50,6 +92,8 @@ int dividir(int a, int b){
     return a/b;
 }
 
+/* === Public function implementation ========================================================== */
+
 /**
  * @brief Función principal del programa.
  * * Crea una instancia de la calculadora abstracta, registra las operaciones básicas
@@ -85,3 +129,5 @@ int main(void){
 
     return 0;
 }
+
+/* === End of documentation ==================================================================== */
